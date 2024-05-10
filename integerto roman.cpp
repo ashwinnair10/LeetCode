9cb4871d29dfecaeed6a901da87a37da8a5f8ba0
ashwinnair10 <ashwinnair1894@gmail.com>
2024-05-10 16:34:48 +0530

    integer to roman

---
+++ integerto roman.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string r="";
+        while(num>=1000){
+            r+='M';
+            num-=1000;
+        }
+        if(num>=900){
+            r.append("CM");
+            num-=900;
+        }
+        if(num>=500){
+            r+='D';
+            num-=500;
+        }
+        if(num>=400){
+            r.append("CD");
+            num-=400;
+        }
+        while(num>=100){
+            r+='C';
+            num-=100;
+        }
+        if(num>=90){
+            r.append("XC");
+            num-=90;
+        }
+        if(num>=50){
+            r+='L';
+            num-=50;
+        }
+        if(num>=40){
+            r.append("XL");
+            num-=40;
+        }
+        while(num>=10){
+            r+='X';
+            num-=10;
+        }
+        if(num>=9){
+            r.append("IX");
+            num-=9;
+        }
+        if(num>=5){
+            r+='V';
+            num-=5;
+        }
+        if(num>=4){
+            r.append("IV");
+            num-=4;
+        }
+        while(num>=1){
+            r+='I';
+            num--;
+        }
+        return r;
+    }
+};
