ec88f19cfe5a1578c539a390773e3f93b489fa2d
ashwinnair10 <ashwinnair1894@gmail.com>
2024-05-20 11:04:52 +0530

    sum of all subset xor totals

---
+++ may20.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int sum=0;
+        for(auto num:nums)
+        sum|=num;
+        return sum<<(nums.size()-1);
+    }
+};
