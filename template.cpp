f6f9c8202599185d145a05f753cf7978c3a5f126
ashwinnair10 <ashwinnair1894@gmail.com>
2024-05-20 16:14:58 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include<limits>
+#include <bits/stdc++.h>
+using namespace std;
+// struct node{
+//     int val;
+//     struct node* next;
+//     node() : val(0), next(nullptr) {}
+//     node(int x) : val(x), next(nullptr) {}
+//     node(int x, node* next) : val(x), next(next) {}
+// };
+// int t;
+// cin<<t;
+// while(t--){
+// }
+// for(auto num:vec){
+// }
+// for(int i=0;i<vec.size();i++){
+// }
+// sort(vec.begin(),vec.end());
+// sort(vec.begin(),vec.end(),greater<int>());
+class Solution{
+    public:
+        // void func(){
+        //     return;
+        // }
+};
+int main(){
+    Solution sol;
+    //sol.func();
+    return 0;
+}
