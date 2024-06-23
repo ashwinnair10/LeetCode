7288848f45c26bf57a1df428b7aba21d34a05247
ashwinnair10 <ashwinnair1894@gmail.com>
2024-06-23 19:32:29 +0530

    modified template

--- template.cpp
+++ template.cpp
@@ -1,31 +1,19 @@
 #include<limits>
 #include <bits/stdc++.h>
 using namespace std;
-// struct node{
-//     int val;
-//     struct node* next;
-//     node() : val(0), next(nullptr) {}
-//     node(int x) : val(x), next(nullptr) {}
-//     node(int x, node* next) : val(x), next(next) {}
-// };
-// int t;
-// cin>>t;
-// while(t--){
-// }
-// for(auto num:vec){
-// }
-// for(int i=0;i<vec.size();i++){
-// }
-// sort(vec.begin(),vec.end());
-// sort(vec.begin(),vec.end(),greater<int>());
 class Solution{
     public:
-        // void func(){
-        //     return;
-        // }
+        void func(){
+            int t;
+            cin>>t;
+            while(t--){
+                
+            }
+            return;
+        }
 };
 int main(){
     Solution sol;
-    //sol.func();
+    sol.func();
     return 0;
 }
