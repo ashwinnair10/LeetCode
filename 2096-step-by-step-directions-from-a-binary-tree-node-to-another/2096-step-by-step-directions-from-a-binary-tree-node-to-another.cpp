145bc65f39c439bd88c216af5157aa1b53ecd677
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2024-07-16 13:09:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<string> direction;
+    void findpath(TreeNode* root,vector<int>& path,string dir,int target,int i){
+        if(root==nullptr)
+        return;
+        if(root->val==target){
+            path.push_back(target);
+            res.push_back(path);
+            direction.push_back(dir);
+            return;
+        }
+        path.push_back(root->val);
+        findpath(root->left,path,dir+'L',target,i);
+        path.pop_back();
+        path.push_back(root->val);
+        findpath(root->right,path,dir+'R',target,i);
+        path.pop_back();
+        return;
+    }
+    string getDirections(TreeNode* root, int s, int t) {
+        vector<int> sp,tp;
+        string sd,td;
+        findpath(root,sp,sd,s,0);
+        findpath(root,tp,td,t,1);
+        cout<<direction[0]<<'\n';
+        cout<<direction[1]<<'\n';
+        int l,r;
+        l=0,r=0;
+        while(l<direction[0].length()&&r<direction[1].length()){
+            if(direction[0][l]==direction[1][r]){
+                l++;
+                r++;
+            }
+            else break;
+        }
+        string ans;
+        for(int j=l;j<direction[0].size();j++)
+        ans+='U';
+        for(int j=r;j<direction[1].size();j++)
+        ans+=direction[1][j];
+        return ans;
+    }
+};
