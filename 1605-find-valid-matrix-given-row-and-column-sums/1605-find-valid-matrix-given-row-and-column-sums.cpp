d89d5bac060694a9b25160b859a1842213c9bbf0
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2024-07-20 10:07:27 +0530

    Time: 41 ms (34.09%), Space: 36 MB (27.54%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,61 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> restoreMatrix(vector<int>& r, vector<int>& c) {
-        vector<vector<int>> a(r.size(),vector<int>(c.size(),0));
-        int m=r.size(),n=a[0].size();
-        for(int j=0;j<m*n;j++){
-        int rm=INT_MAX,rmi,cm=INT_MAX,cmi;
-        for(int i=0;i<r.size();i++){
-            if(r[i]&&r[i]<rm){
-                rm=r[i];
-                rmi=i;
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int rows = rowSum.size();
+        int col = colSum.size();
+        vector<vector<int>>ans (rows, vector<int>(col));
+        for (int i = 0 ; i < rows ; i++){
+            for (int j = 0 ; j< col ;j++){
+                ans[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] = rowSum[i] - ans[i][j];
+                colSum[j] = colSum[j] - ans[i][j];
             }
         }
-        for(int i=0;i<c.size();i++){
-            if(c[i]&&c[i]<cm){
-                cm=c[i];
-                cmi=i;
-            }
-        }
-        if(rm==0&&cm==0)
-        break;
-        //cout<<"rm : "<<rm<<" rmi: "<<rmi<<" cm: "<<cm<<" cmi: "<<cmi<<'\n';
-        if(rm<cm){
-            for(int i=0;i<n;i++){
-                if(c[i]&&!a[rmi][i]){
-                    a[rmi][i]=rm;
-                    c[i]-=rm;
-                    r[rmi]-=rm;
-                    break;
-                }
-            }
-        }
-        else{
-            for(int i=0;i<m;i++){
-                if(r[i]&&!a[i][cmi]){
-                    a[i][cmi]=cm;
-                    r[i]-=cm;
-                    c[cmi]-=cm;
-                    break;
-                }
-            }
-        }
-        // cout<<"Matrix\n";
-        // for(int k=0;k<m;k++){
-        //     for(int l=0;l<n;l++){
-        //         cout<<a[k][l]<<' ';
-        //     }
-        //     cout<<'\n';
-        // }
-        // cout<<"ROW\n";
-        // for(auto i:r)
-        // cout<<i<<' ';
-        // cout<<'\n';
-        // cout<<"COLUMN\n";
-        // for(auto i:c)
-        // cout<<i<<' ';
-        // cout<<'\n';
-        }
-        return a;
+        return ans ; 
     }
 };
