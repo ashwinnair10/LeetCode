56e7f9db07ff3f07aed9a2c856423f4185ce7978
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2024-07-22 10:43:55 +0530

    Time: 2 ms (43.99%), Space: 10.1 MB (9.59%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int l=0,m=0,r=a.size()-1;
+        while(m<=r){
+            if(a[m]==0){
+                swap(a[l],a[m]);
+                l++;
+                m++;
+            }
+            else if(a[m]==1)
+            m++;
+            else{
+                swap(a[m],a[r]);
+                r--;
+            }
+        }
+        return;
+    }
+};
