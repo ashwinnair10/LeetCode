3777decd879b0ab65e396ad6f2ba44b79529d9e0
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2024-07-27 11:19:03 +0530

    Time: 405 ms (12.83%), Space: 79.5 MB (24.72%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    int ans=INT_MAX;
-    string change(string s,int ind,int p){
-        if(p){
-            if(s[ind]<'9')
-            s[ind]=(s[ind]+1);
-            else
-            s[ind]='0';
-        }
-        else{
-            if(s[ind]>'0')
-            s[ind]=s[ind]-1;
-            else
-            s[ind]='9';
+    int ans = INT_MAX;
+    string change(string s, int ind, int p) {
+        if (p) {
+            s[ind] = (s[ind] == '9') ? '0' : s[ind] + 1;
+        } else {
+            s[ind] = (s[ind] == '0') ? '9' : s[ind] - 1;
         }
         return s;
     }
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string> visit;
-        for(auto i:deadends){
-            visit.insert(i);
-        }
-        string s="0000";
-        if(visit.find(s)!=visit.end())
-        return -1;
-        int j=0;
-        queue<pair<string,int>> q;
-        q.push({s,0});
-        while(!q.empty()){
-            string curr=q.front().first;
-            int c=q.front().second;
+        unordered_set<string> visit(deadends.begin(), deadends.end());
+        if (visit.find("0000") != visit.end())
+            return -1;
+        queue<pair<string, int>> q;
+        q.push({"0000", 0});
+        visit.insert("0000");
+        while (!q.empty()) {
+            string curr = q.front().first;
+            int c = q.front().second;
             q.pop();
-            if(curr==target){
+            if (curr == target) {
                 ans=min(ans,c);
                 continue;
             }
-            int j=0;
-            int n=4,l=0;
-            for(int i=j%4,l=0;l<4;l++,i=(i+1)%4){
-                string s1=change(curr,i,0);
-                string s2=change(curr,i,1);
-                if(visit.find(s2)==visit.end()){
-                    q.push({s2,c+1});
-                    visit.insert(s2);
-                }
-                if(visit.find(s1)==visit.end()){
-                    q.push({s1,c+1});
+            for (int i = 0; i < 4; ++i) {
+                string s1 = change(curr, i, 0);
+                string s2 = change(curr, i, 1);
+
+                if (visit.find(s1) == visit.end()) {
+                    q.push({s1, c + 1});
                     visit.insert(s1);
+                }
+                if (visit.find(s2) == visit.end()) {
+                    q.push({s2, c + 1});
+                    visit.insert(s2);
                 }
             }
         }
