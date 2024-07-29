d0dad7db9a7672ec34dcbfb74b79e83dce456f87
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2024-07-29 21:28:52 +0530

    Time: 67 ms (99.53%), Space: 61 MB (95.05%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        backtrack(1, comb, res, n, k);
-        return res;        
-    }
-    void backtrack(int start, vector<int>& comb, vector<vector<int>>& res, int n, int k) {
-        if (comb.size() == k) {
-            res.push_back(comb);
+    void backtrack(vector<vector<int>>& ans,vector<int>& v,vector<int>& a,int ind,int count,int k){
+        v.push_back(a[ind]);
+        count++;
+        if(count==k){
+            ans.push_back(v);
+            v.pop_back();
+            count--;
             return;
         }
-        for (int num = start; num <= n; num++) {
-            comb.push_back(num);
-            backtrack(num + 1, comb, res, n, k);
-            comb.pop_back();
+        for(int i=ind+1;i<a.size();i++){
+            backtrack(ans,v,a,i,count,k);
         }
-    }    
+        v.pop_back();
+        count--;
+        return;
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> a;
+        vector<vector<int>> ans;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        a.push_back(i+1);
+        for(int i=0;i<n;i++){
+            backtrack(ans,v,a,i,0,k);
+        }
+        return ans;
+    }
 };
