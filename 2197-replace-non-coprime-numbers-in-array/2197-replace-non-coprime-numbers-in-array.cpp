ac14e46c26905d3bf77d0b898eeca8cd5c811231
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2025-09-17 08:51:56 +0530

    Time: 17 ms (71.43%), Space: 125.2 MB (64.98%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> stack;
+        for (int num : nums) {
+            while (!stack.empty()) {
+                long long top = stack.back();
+                int g = gcd(top, num);
+                if (g == 1) {
+                    break;
+                }
+                stack.pop_back();
+                long long lcm = (long long)(top*num) / g;
+                num = (int)lcm;
+            }
+            stack.push_back(num);
+        }
+        return stack;
+    }
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+};
