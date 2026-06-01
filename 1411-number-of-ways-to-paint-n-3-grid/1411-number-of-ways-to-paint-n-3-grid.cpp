b7ce75f6fbe428fa17a3804b8dfd47a6848d1c55
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-01 15:54:34 +0530

    Time: 11 ms (32.99%), Space: 14.2 MB (22.09%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long test(int ind,int n,int type,vector<vector<long long>>& dp){
+        long long l,r;
+        if(ind>n)
+            return 1;
+        if(dp[ind][type]!=-1){
+            return dp[ind][type];
+        }
+        if(ind==1){
+            l=6;
+            r=6;
+        }
+        else{
+            if(type==0){
+                l=2;
+                r=2;
+            }
+            else{
+                l=3;
+                r=2;
+            }
+        }
+        return dp[ind][type]=(l*test(ind+1,n,1,dp)+r*test(ind+1,n,0,dp))%1000000007;
+    }
+    int numOfWays(int n) {
+        vector<vector<long long>> dp(n+1,vector<long long>(2,-1));
+        int ans=(int)(test(1,n,0,dp))%1000000007;
+        return ans;
+    }
+};
