c73883b1d4ab7422def23e2bb9073152f605491f
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-06 13:10:26 +0530

    Time: 0 ms (100%), Space: 12.2 MB (96.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     void merge(vector<int>& a, int m, vector<int>& b, int n) {
-        vector<int> ans;
-        int i=0,j=0;
-        while(i<m&&j<n){
-            if(a[i]<=b[j]){
-                ans.push_back(a[i]);
-                i++;
+        int i=m-1,j=n-1,k=m+n-1;
+        while(j>=0){
+            if(i>=0&&a[i]>=b[j]){
+                a[k--]=a[i--];
             }
             else{
-                ans.push_back(b[j]);
-                j++;
+                a[k--]=b[j--];
             }
-        }
-        while(i<m){
-            ans.push_back(a[i]);
-            i++;
-        }
-        while(j<n){
-            ans.push_back(b[j]);
-            j++;
-        }
-        for(int i=0;i<m+n;i++){
-            a[i]=ans[i];
         }
     }
 };
