574758447bfa9143afdbd716d57bd2ae459149c1
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-06 15:04:51 +0530

    Time: 72 ms (17.34%), Space: 21.8 MB (12.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int m=INT_MAX;
-    int test(vector<int>& a,vector<int>& dp,int ind,int n){
-        if(ind>=n-1){
-            return 0;
-        }
-        else if(dp[ind]!=99999){
-            return dp[ind];
-        }
-        for(int i=1;i<=a[ind];i++){
-            dp[ind]=min(dp[ind],1+test(a,dp,ind+i,n));
-        }
-        return dp[ind];
-    }
     int jump(vector<int>& a) {
         int n=a.size();
         vector<int> dp(n,99999);
-        return test(a,dp,0,n);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=1;j<=a[i];j++){
+                dp[i]=min(dp[i],1+dp[min(n-1,i+j)]);
+            }
+        }
+        return dp[0];
     }
 };
