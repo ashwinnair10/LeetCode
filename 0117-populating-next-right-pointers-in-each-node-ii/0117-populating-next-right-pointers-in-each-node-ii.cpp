b23cefc0b2452e864193ca09d1c8accb895d306b
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-06 19:03:51 +0530

    Time: 12 ms (29.32%), Space: 20.4 MB (5.07%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,21 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
     void levelTraversal(Node* root,vector<vector<Node*>>& level,int l){
@@ -34,10 +16,8 @@ public:
         levelTraversal(root,level,0);
         for(auto i:level){
             for(int j=0;j<i.size()-1;j++){
-                // cout<<i[j]->val<<" ";
                 i[j]->next=i[j+1];
             }
-            // cout<<"\n";
         }
         return root;
     }
