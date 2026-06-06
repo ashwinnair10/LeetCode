2b0379e82900f2eecebe014a40809ed126f49cff
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-06 15:11:00 +0530

    Time: 5 ms (8.63%), Space: 47.1 MB (5.89%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> p,s;
+        int n=nums.size();
+        int a=1,b=1;
+        for(int i=0;i<n;i++){
+            p.push_back(a);
+            a*=nums[i];
+            s.push_back(b);
+            b*=nums[n-1-i];
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(p[i]*s[n-1-i]);
+        }
+        return ans;
+    }
+};
