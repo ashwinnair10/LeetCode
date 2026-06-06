2a04a7f40323ce606c9df3e56db484057f6cd63e
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-06 16:37:06 +0530

    Time: 0 ms (100%), Space: 10.3 MB (6.76%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,56 +6,31 @@ public:
     }
     
     void push(int x) {
-        if(s1.empty()){
-            while(!s2.empty()){
-                int k=s2.top();
-                s2.pop();
-                s1.push(k);
-            }
-            s1.push(x);
-            while(!s1.empty()){
-                int k=s1.top();
-                s1.pop();
-                s2.push(k);
-            }
+        while(!s2.empty()){
+            int k=s2.top();
+            s2.pop();
+            s1.push(k);
         }
-        else if(s2.empty()){
-            while(!s1.empty()){
-                int k=s1.top();
-                s1.pop();
-                s2.push(k);
-            }
-            s2.push(x);
-            while(!s2.empty()){
-                int k=s2.top();
-                s2.pop();
-                s1.push(k);
-            }
+        s1.push(x);
+        while(!s1.empty()){
+            int k=s1.top();
+            s1.pop();
+            s2.push(k);
         }
     }
     
     int pop() {
-        int k;
-        if(s1.empty()){
-            k=s2.top();
-            s2.pop();
-        }
-        else{
-            k=s1.top();
-            s1.pop();
-        }
+        int k=s2.top();
+        s2.pop();
         return k;
     }
     
     int peek() {
-        if(s1.empty())
         return s2.top();
-        else
-        return s1.top();
     }
     
     bool empty() {
-        return s1.empty()&&s2.empty();
+        return s2.empty();
     }
 };
 
