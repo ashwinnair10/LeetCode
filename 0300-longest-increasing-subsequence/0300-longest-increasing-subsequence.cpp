4eac771e7015071dfeb8e0114c64a1e5fe23c851
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-07 23:48:37 +0530

    Time: 2 ms (78.19%), Space: 13.9 MB (93.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size(),ans=1;
-        vector<int> dp(n,1);
-        for(int i=n-1;i>=0;i--){
-            for(int j=i+1;j<n;j++){
-                if(nums[j]>nums[i]){
-                    dp[i]=max(dp[i],dp[j]+1);
-                }
+        vector<int> tails;
+        for(auto i:nums){
+            auto it=lower_bound(tails.begin(),tails.end(),i);
+            if(it==tails.end()){
+                tails.push_back(i);
             }
-            ans=max(ans,dp[i]);
+            else{
+                *it=i;
+            }
         }
-        return ans;
+        return tails.size();
     }
 };
