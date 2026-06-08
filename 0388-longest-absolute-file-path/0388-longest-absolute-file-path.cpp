bd609a02790d4a6e4a28fa74d4f43f6dbf4114cf
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-08 18:52:48 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.74%) - LeetHub

---
+++ 0388-longest-absolute-file-path/0388-longest-absolute-file-path.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        stack<string> st;
+        stack<int> level;
+        int ans=0,len=0;
+        string curr="";
+        for(int i=0;i<input.length();i++){
+            if(input[i]=='\n'){
+                cout<<curr<<"\n";
+                int tabs=0;
+                while(curr[tabs]=='\t')
+                tabs++;
+                while(!st.empty()&&level.top()>=tabs){   
+                    len-=st.top().length();
+                    if(st.top().find(".")==string::npos)
+                    len--;
+                    st.pop();
+                    level.pop();
+                }
+                st.push(curr.substr(tabs));
+                level.push(tabs);
+                len+=st.top().length();
+                if(st.top().find(".")==string::npos)
+                len++;
+                else
+                ans=max(ans,len);
+                curr="";
+            }
+            else{
+                curr+=input[i];
+            }
+        }
+        int tabs=0;
+        while(curr[tabs]=='\t')
+        tabs++;
+        while(!st.empty()&&level.top()>=tabs){   
+            len-=st.top().length();
+            if(st.top().find(".")==string::npos)
+            len--;
+            st.pop();
+            level.pop();
+        }
+        st.push(curr.substr(tabs));
+        level.push(tabs);
+        len+=st.top().length();
+        if(st.top().find(".")==string::npos)
+        len++;
+        else
+        ans=max(ans,len);
+        curr="";
+        return ans;
+    }
+};
