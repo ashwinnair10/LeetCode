5263d03bc79934d816402d56b071803e6ec54441
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-09 21:17:29 +0530

    Time: 27 ms (3.66%), Space: 24.2 MB (42.54%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        if(f.size()==1){
+            return (n==1&&f[0]==0)||n==0;
+        }
+        int c=0;
+        if(!f[0]&&!f[1]){
+            cout<<"hi"<<"\n";
+            c++;
+            f[0]=1;
+        }
+        for(int i=1;i<f.size()-1;i++){
+            if(!f[i]&&!f[i-1]&&!f[i+1]){
+                cout<<i<<"\n";
+                f[i]=1;
+                c++;
+            }
+        }
+        if(!f[f.size()-2]&&!f[f.size()-1]){
+            c++;
+            f[f.size()-1]=1;
+        }
+        return c>=n;
+    }
+};
