044ced9d2cb0d4256a9dda735b99889fb8212aa4
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-10 11:44:01 +0530

    Time: 11 ms (75.12%), Space: 39.4 MB (25.11%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -8,17 +8,17 @@ public:
         return ans;
     }
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string,int> map;
+        map<vector<int>,int> map;
         int ans=0;
         for(auto i:grid){
-            map[tostring(i)]++;
+            map[i]++;
         }
         for(int j=0;j<grid[0].size();j++){
             vector<int> v;
             for(int i=0;i<grid.size();i++){
                 v.push_back(grid[i][j]);
             }
-            ans+=map[tostring(v)];
+            ans+=map[v];
         }
         return ans;
     }
