9d942a7fff81d65fa454d970bc2c2cf57441fbd1
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-11 21:06:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (70.95%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& a) {
-        a.insert(a.begin(),INT_MIN);
-        a.push_back(INT_MIN);
-        int l=1,r=a.size()-1,m;
-        while(l<=r){
+        int l=0,r=a.size()-1,m;
+        while(l<r){
             m=l+(r-l)/2;
-            if(a[m]>a[m-1]&&a[m]>a[m+1])
-            return m-1;
-            else if(a[m-1]<a[m])
+            if(a[m]<a[m+1])
             l=m+1;
             else
-            r=m-1;
+            r=m;
         }
-        return 0;
+        return l;
     }
 };
