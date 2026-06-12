13bab4ab529340c0701812fe57b172888f86df44
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-12 11:36:03 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int test(vector<int>& a,vector<int>& dp,int ind,int n){
-        if(ind>=n)
-        return 0;
-        if(dp[ind]!=INT_MIN)
-        return dp[ind];
-        dp[ind]=max(test(a,dp,ind+1,n),a[ind]+test(a,dp,ind+2,n));
-        return dp[ind];
-    }
-    int rob(vector<int>& a) {
-        int n=a.size();
-        vector<int> dp(n,INT_MIN);
-        return test(a,dp,0,n);
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
+        return nums[0];
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        int m=max(dp[n-1],dp[n-2]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 };
