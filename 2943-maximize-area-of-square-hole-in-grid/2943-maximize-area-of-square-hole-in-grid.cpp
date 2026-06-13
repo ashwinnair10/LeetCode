8ed4a0895c26dd1367056a439d0641a43867af8d
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-13 14:23:58 +0530

    Time: 2 ms (14.64%), Space: 31.7 MB (94.75%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -5,13 +5,9 @@ public:
         vBars.push_back(1);
         sort(hBars.begin(),hBars.end());
         sort(vBars.begin(),vBars.end());
-
         int sh=1,msh=0,i;
         for(i=1;i<hBars.size();i++){
-            if(hBars[i]==hBars[i-1]+1){
-
-            }
-            else{
+            if(hBars[i]!=hBars[i-1]+1){
                 msh=max(msh,hBars[i-1]+1-sh);
                 sh=hBars[i]-1;
             }
@@ -19,10 +15,7 @@ public:
         msh=max(msh,hBars[i-1]+1-sh);
         int sv=1,msv=0;
         for(i=1;i<vBars.size();i++){
-            if(vBars[i]==vBars[i-1]+1){
-
-            }
-            else{
+            if(vBars[i]!=vBars[i-1]+1){
                 msv=max(msv,vBars[i-1]+1-sv);
                 sv=vBars[i]-1;
             }
