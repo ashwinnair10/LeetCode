fe25e77ca14fb60b155645f34673f9763a5aff21
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-13 14:22:23 +0530

    Time: 2 ms (14.64%), Space: 31.9 MB (68.51%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
         hBars.push_back(1);
-        // hBars.push_back(n+2);
         vBars.push_back(1);
-        // vBars.push_back(m+2);
         sort(hBars.begin(),hBars.end());
         sort(vBars.begin(),vBars.end());
 
@@ -17,16 +15,8 @@ public:
                 msh=max(msh,hBars[i-1]+1-sh);
                 sh=hBars[i]-1;
             }
-            cout<<"i : "<<i<<" , hBars[i] : "<<hBars[i]<<" , msh : "<<msh<<"\n";
         }
         msh=max(msh,hBars[i-1]+1-sh);
-        // cout<<"i : "<<i<<" , hBars[i] : "<<hBars[i]<<" , msh : "<<msh<<"\n";
-        
-
-
-
-        cout<<"Max horizontal side : "<<msh<<"\n";
-        
         int sv=1,msv=0;
         for(i=1;i<vBars.size();i++){
             if(vBars[i]==vBars[i-1]+1){
@@ -36,14 +26,8 @@ public:
                 msv=max(msv,vBars[i-1]+1-sv);
                 sv=vBars[i]-1;
             }
-            cout<<"i : "<<i<<" , vBars[i] : "<<vBars[i]<<" , msv : "<<msv<<"\n";
         }
         msv=max(msv,vBars[i-1]+1-sv);
-        // cout<<"i : "<<i<<" , vBars[i] : "<<vBars[i]<<" , msv : "<<msv<<"\n";
-
-
-
-        cout<<"Max vertical side : "<<msv<<"\n";
         int s=min(msh,msv);
         return s*s;
     }
