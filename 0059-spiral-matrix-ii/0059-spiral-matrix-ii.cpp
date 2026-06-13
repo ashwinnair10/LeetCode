a65e1a19d6ac551d86d04267a32a489dba020072
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-13 19:42:36 +0530

    Time: 0 ms (100%), Space: 9.4 MB (40.6%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans(n,vector<int>(n));
+        int ru=0,rl=n-1,cu=0,cl=n-1;
+        int val=1;
+        while(ru<=rl&&cu<=cl){
+            for(int i=cu;i<=cl;i++){
+                ans[ru][i]=val;
+                val++;
+            }
+            ru++;
+            for(int i=ru;i<=rl;i++){
+                ans[i][cl]=val;
+                val++;
+            }
+            cl--;
+            for(int i=cl;i>=cu;i--){
+                ans[rl][i]=val;
+                val++;
+            }
+            rl--;
+            for(int i=rl;i>=ru;i--){
+                ans[i][cu]=val;
+                val++;
+            }
+            cu++;
+        }
+        return ans;
+    }
+};
