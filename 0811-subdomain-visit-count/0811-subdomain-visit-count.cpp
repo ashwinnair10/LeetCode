11064969087a73589207ec652ef23b16e9d76f7e
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-16 16:18:50 +0530

    Time: 3 ms (90.05%), Space: 16 MB (77.91%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+        unordered_map<string,int> map;
+        for(auto i:cpdomains){
+            string s="";
+            int n=0;
+            for(int j=0;j<i.length();j++){
+                if(i[j]==' ')
+                break;
+                n=(n*10)+(i[j]-'0');
+                // cout<<"n : "<<n<<"\n";
+            }
+            string d="";
+            for(int j=i.length()-1;j>=0;j--){
+                if(i[j]=='.'){
+                    reverse(s.begin(),s.end());
+                    d=s+d;
+                    map[d]+=n;
+                    s="";
+                }
+                else if(i[j]==' '){
+                    reverse(s.begin(),s.end());
+                    d=s+d;
+                    map[d]+=n;
+                    s="";
+                    break;
+                }
+                s+=i[j];
+            }
+        }
+        vector<string> ans;
+        for(auto i:map){
+            ans.push_back(to_string(i.second)+" "+i.first);
+        }
+        return ans;
+    }
+};
