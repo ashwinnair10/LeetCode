023029aaad7e5e7296ec6594822d278ed637f159
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-17 21:44:16 +0530

    Time: 0 ms (100%), Space: 30 MB (29.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,8 +6,6 @@ public:
         int c=k-1,i=nums.size()-1;
         while(c>=0)
         r[c--]=nums[i--];
-        for(auto j:r)
-        cout<<j<<" ";
         for(i=nums.size()-1;i>=k;i--)
         nums[i]=nums[i-k];
         for(i=0;i<k;i++)
