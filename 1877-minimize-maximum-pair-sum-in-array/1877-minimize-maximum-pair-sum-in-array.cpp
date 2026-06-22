5b67e252b2c1039768341e21d231dd68866dfde8
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-06-22 13:09:02 +0530

    Time: 187 ms (7.77%), Space: 99.9 MB (85.1%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -3,22 +3,9 @@ public:
     int minPairSum(vector<int>& nums) {
         int ans=INT_MIN;
         sort(nums.begin(),nums.end());
-        vector<pair<int,int>> v;
         int l=0,r=nums.size()-1;
         while(l<r){
-            // while(l+1<=r&&nums[l]==nums[l+1]){
-            //     ans=max(ans,nums[l]*2);
-            //     cout<<"ans : "<<ans<<"\n";
-            //     l+=2;
-            // }
-            // while(r-1>=l&&nums[r]==nums[r-1]){
-            //     ans=max(ans,nums[r]*2);
-            //     cout<<"ans : "<<ans<<"\n";
-            //     r-=2;
-            // }
-            // if(l<r)
             ans=max(ans,nums[l]+nums[r]);
-            cout<<"ans : "<<ans<<"\n";
             l++;
             r--;
         }
