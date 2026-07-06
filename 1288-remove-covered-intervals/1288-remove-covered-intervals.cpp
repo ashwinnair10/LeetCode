fecf8f8bf9b8aa57dad116dcb772f34c7b5cc6b5
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-07-06 09:49:32 +0530

    Time: 3 ms (42.52%), Space: 15.3 MB (20.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -5,8 +5,6 @@ public:
     }
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),cmp);
-        for(auto i:intervals)
-        cout<<i[0]<<" -- "<<i[1]<<"\n";
         int ans=intervals.size();
         int s=intervals[0][0],e=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
