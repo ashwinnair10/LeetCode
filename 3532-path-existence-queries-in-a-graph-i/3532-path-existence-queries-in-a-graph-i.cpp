cfa8cb0bee042c887ea74d31b11f30261c7136e6
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-07-09 20:25:59 +0530

    Time: 5 ms (86.58%), Space: 224 MB (90.41%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj,int& ind,int& par,vector<int>& part,int& t){
-        part[ind]=t;
-        for(auto i:adj[ind]){
-            if(i!=par){
-                dfs(adj,i,ind,part,t);
-            }
-        }
-    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n-1;i++){
-            if(abs(nums[i]-nums[i+1])<=maxDiff){
-                adj[i].push_back(i+1);
-                adj[i+1].push_back(i);
+        vector<int> group(n, 0);
+        int currentGroup = 0;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                currentGroup++;
             }
+            group[i] = currentGroup;
         }
-        vector<int> part(n,-1);
-        int t=0,p=-1;
-        for(int i=0;i<n;i++){
-            if(part[i]==-1){
-                dfs(adj,i,p,part,t);
-                t++;
-            }
+
+        vector<bool> res;
+        for (auto& q : queries) {
+            int u = q[0], v = q[1];
+            res.push_back(group[u] == group[v]);
         }
-        vector<bool> ans;
-        for(auto i:queries)
-        ans.push_back(part[i[0]]==part[i[1]]);
-        return ans;
+        return res;
     }
 };
