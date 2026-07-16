845372db7ecf235686899ae074074983c9d4a437
Ashwin A Nair <118875071+ashwinnair10@users.noreply.github.com>
2026-07-16 13:05:54 +0530

    Time: 76 ms (50.08%), Space: 163 MB (20.28%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -14,9 +14,6 @@ public:
             m=max(m,nums[i]);
             prefixGcd.push_back(gcd(m,nums[i]));
         }
-        for(auto i:prefixGcd)
-        cout<<i<<" ";
-        cout<<"\n";
         sort(prefixGcd.begin(),prefixGcd.end());
         long long ans=0;
         int l=0,r=prefixGcd.size()-1;
